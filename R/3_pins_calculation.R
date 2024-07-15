589396a079ed98b8a2e12170082e3d01de970dfe
Marketa Jerabek <102174405+Marki4@users.noreply.github.com>
2024-07-15 14:20:16 -0500

    Update 3_pins_calculation.R

--- R/3_pins_calculation.R
+++ R/3_pins_calculation.R
@@ -2,6 +2,14 @@ pins_indicator <- main_merged %>%
   #Select only the previously calculated indicators
   select(
     c(
+      #Sex (all members)
+      MH_4,
+      #Age1 years (all members)
+      age1, 
+      #Age2 months (all members)
+      age2, 
+      # pregnant women
+      NUT_D1_Q1,
       #Food Security
       IND_FS_max,
       #Health
@@ -33,9 +41,17 @@ pins_indicator <- main_merged %>%
       IND_WA_D3_max,
       #Education
       IND_EDU_D1_max,
-      IND_EDU_D2_max
-    )
-  )
+      IND_EDU_D2_max))%>%
+      mutate(age1_rounded = floor(age1),
+         age2_rounded = floor(age2),
+         child_edu_cp=case_when(
+           age1_rounded>=0& age1_rounded<=17~"Yes",
+           TRUE~"No"),
+         child_0_5=case_when(
+           age1_rounded>=0& age1_rounded<=5~"Yes",
+           TRUE~"No"),
+         woman_preg_lact=case_when(MH_4=="women" & age1_rounded>=14 & (NUT_D1_Q1=="breastfeeding" |NUT_D1_Q1=="pregnant")~"Yes",
+                                   TRUE~"No"))
 
 # read indicators sheet with weights
 
@@ -44,7 +60,16 @@ pins_indicator <- main_merged %>%
 #Multiply each column of pins_indicator by the respective weight in df_weights
 
 weighted_pins_indicator <- pins_indicator |>
-  mutate(across(everything(), ~ . * df_weights$ind_weight_per_sector[match(cur_column(), df_weights$ind_per_sector)]))
+  mutate(across(c(IND_FS_max,
+                  IND_HE_max,
+                  IND_HT_max, 
+                  IND_INT_D1_max,IND_INT_D2_max, IND_INT_D3_max,
+                  IND_NUT_D1,IND_NUT_D2,IND_NUT_D3,
+                  IND_PRO_D1_max,IND_PRO_D2_max,IND_PRO_D3_max,IND_PRO_CP_max,IND_PRO_GBV_max,IND_PRO_HTS_max,
+                  IND_SHE_D1_max, IND_SHE_D2_max,IND_SHE_D3_max,
+                  IND_WA_D1_max, IND_WA_D2_max,IND_WA_D3_max,
+                  IND_EDU_D1_max, IND_EDU_D2_max), ~ . * 
+                  df_weights$ind_weight_per_sector[match(cur_column(), df_weights$ind_per_sector)]))
 
 #Calculate sectorial deprivation score ds suming weighted indicators per sector
 
@@ -55,6 +80,8 @@ ds_per_sector <- weighted_pins_indicator %>%
     Humanitarian_transport_ds = IND_HT_max,
     Integration_ds = rowSums(weighted_pins_indicator[, c("IND_INT_D1_max", "IND_INT_D2_max", "IND_INT_D3_max")], na.rm = TRUE),
     Nutrition_ds = rowSums(weighted_pins_indicator[, c("IND_NUT_D1","IND_NUT_D2", "IND_NUT_D3" )], na.rm = TRUE),
+    Nutrition_ds_women = rowSums(weighted_pins_indicator[, c("IND_NUT_D1" )], na.rm = TRUE),
+    Nutrition_ds_baby = rowSums(weighted_pins_indicator[, c("IND_NUT_D2", "IND_NUT_D3" )], na.rm = TRUE),
     Protection_ds = rowSums(weighted_pins_indicator[, c("IND_PRO_D1_max", "IND_PRO_D2_max", "IND_PRO_D3_max", "IND_PRO_CP_max", "IND_PRO_GBV_max","IND_PRO_HTS_max")], na.rm = TRUE),
     Child_protection_ds = IND_PRO_CP_max,
     Gender_based_violence_ds = IND_PRO_GBV_max,
@@ -70,19 +97,6 @@ ds_per_sector <- weighted_pins_indicator %>%
 #Exclude subsectors - CP, HT and GBV (only calculate intersectoral MPI with 9 dimensions)
 
 df_pin <- ds_per_sector %>%
-  select(
-      Food_security_ds,
-      Health_ds,
-      Humanitarian_transport_ds,
-      Integration_ds,
-      Nutrition_ds,
-      Protection_ds,
-      Child_protection_ds,
-      Gender_based_violence_ds,
-      HT_S_ds,
-      Shelter_ds,
-      Wash_ds,
-      Education_ds)%>%
   mutate(intersector_ds = rowSums(select(., c(
     "Food_security_ds",
     "Health_ds",
@@ -122,11 +136,9 @@ df_sectoral_pin <- df_pin %>%
       "Health_ds",
       "Humanitarian_transport_ds",
       "Integration_ds",
-      "Nutrition_ds",
       "Protection_ds",
       "Shelter_ds",
-      "Wash_ds",
-      "Education_ds"
+      "Wash_ds"
     ),
     ~ ifelse(intersector_pin == 1 & . > 0, 1, 0)
   )) |>
@@ -136,14 +148,11 @@ df_sectoral_pin <- df_pin %>%
                 "Health_ds",
                 "Humanitarian_transport_ds",
                 "Integration_ds",
-                "Nutrition_ds",
                 "Protection_ds",
                 "Shelter_ds",
-                "Wash_ds",
-                "Education_ds")) %>%
+                "Wash_ds")) %>%
   mutate(across(
     c(
-      "Child_protection_ds",
       "Gender_based_violence_ds",
       "HT_S_ds"
     ),
@@ -153,28 +162,134 @@ df_sectoral_pin <- df_pin %>%
   rename_with( ~ str_replace(., "_ds$", "_pin")) 
 
 
+#Education
+
+df_sectoral_pin_edu <- df_pin %>%
+  filter(child_edu_cp=="Yes")%>%
+  select(-intersector_ds, Education_ds) %>%
+  #include in sectorial pins if included in intersectorial pin (intersector== 1) 
+  #and there is a privation in the specific sector (sector_ds>0)
+  mutate(across(
+    c(
+      "Education_ds"
+    ),
+    ~ ifelse(intersector_pin == 1 & . > 0, 1, 0)
+  )) |>
+  #rename ds to pin
+  rename_with( ~ str_replace(., "_ds$", "_pin")) 
+
+#final Education pin
+pins_final_edu <- data.frame(  
+  Sectors = c(
+   "Education"
+  ),
+  pins = round(colSums(select(df_sectoral_pin_edu, Education_pin)) / nrow(df_sectoral_pin_edu) *
+                 100, 1)
+)
+
+
+#Nutrition women
+df_sectoral_pin_nut_w <- df_pin %>%
+  filter(woman_preg_lact=="Yes")%>%
+  select(-intersector_ds, Nutrition_ds_women) %>%
+  #include in sectorial pins if included in intersectorial pin (intersector== 1) 
+  #and there is a privation in the specific sector (sector_ds>0)
+  mutate(across(
+    c(
+      "Nutrition_ds_women"
+    ),
+    ~ ifelse(intersector_pin == 1 & . > 0, 1, 0)
+  )) |>
+  #rename ds to pin
+  rename_with( ~ str_replace(., "_ds_", "_pin_")) 
+
+#final Nutrition women pin
+pins_final_nut_w <- data.frame(  
+  Sectors = c(
+    "Nutrition Woman"
+  ),
+  pins = round(colSums(select(df_sectoral_pin_nut_w, Nutrition_pin_women)) / nrow(df_sectoral_pin_nut_w) *
+                 100, 1)
+)
+
+
+#Nutrition 0-5 years
+df_sectoral_pin_nut_b <- df_pin %>%
+  filter(child_0_5=="Yes")%>%
+  select(-intersector_ds, Nutrition_ds_baby) %>%
+  #include in sectorial pins if included in intersectorial pin (intersector== 1) 
+  #and there is a privation in the specific sector (sector_ds>0)
+  mutate(across(
+    c(
+      "Nutrition_ds_baby"
+    ),
+    ~ ifelse(intersector_pin == 1 & . > 0, 1, 0)
+  )) |>
+  #rename ds to pin
+  rename_with( ~ str_replace(., "_ds_", "_pin_")) 
+
+#final Nutrition baby pin
+pins_final_nut_b <- data.frame(  
+  Sectors = c(
+    "Nutrition Baby"
+  ),
+  pins = round(colSums(select(df_sectoral_pin_nut_b, Nutrition_pin_baby)) / nrow(df_sectoral_pin_nut_b) *
+                 100, 1)
+)
+
+#Child Protection
+df_sectoral_pin_cp <- df_sectoral_pin %>%
+  filter(child_edu_cp=="Yes")%>%
+  #include in sectorial pins if included in intersectorial pin (intersector== 1) 
+  #and there is a privation in the specific sector (sector_ds>0)
+  mutate(across(
+    c(
+      "Child_protection_pin"
+    ),
+    ~ ifelse(Protection_pin == 1 & . > 0, 1, 0)
+  )) |>
+  #rename ds to pin
+  rename_with( ~ str_replace(., "_ds$", "_pin")) 
+
+#final Nutrition baby pin
+pins_final_cp <- data.frame(  
+  Sectors = c(
+    "Child Protection"
+  ),
+  pins = round(colSums(select(df_sectoral_pin_cp, Child_protection_pin)) / nrow(df_sectoral_pin_cp) *
+                 100, 1)
+)
+
+
 #final PIN table
-pins_final <- data.frame(
+pins_final <- data.frame(  
   Sectors = c(
     "Food_security",
     "Health",
     "Humanitarian_transport",
     "Integration",
-    "Nutrition",
     "Protection",
-    "Child_protection",
     "Gender_based_violence",
     "Human_Traficking_&_Smuggling",
     "Shelter",
     "Wash",
-    "Education",
     "Intersector"
   ),
-  pins = round(colSums(df_sectoral_pin) / nrow(df_sectoral_pin) *
+  pins = round(colSums(select(df_sectoral_pin, Food_security_pin, Health_pin, 
+                              Humanitarian_transport_pin, Integration_pin,
+                              Protection_pin, Gender_based_violence_pin,
+                              HT_S_pin,
+                              Shelter_pin, Wash_pin, intersector_pin)) / nrow(df_sectoral_pin) *
                  100, 1)
 )
 
-write_xlsx(pins_final, "pins_final.xlsx")
+#merge EDU, NUT, CP and final
+
+#merge
+
+pins_final_final <- rbind(pins_final, pins_final_edu, pins_final_nut_w, pins_final_nut_b, pins_final_cp)
+
+write_xlsx(pins_final_final, "pins_final_final.xlsx")
 
 
 write_xlsx(pins_final, "rmna_pin2025.xlsx")
