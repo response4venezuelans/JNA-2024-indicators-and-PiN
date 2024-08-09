09afbee99ae08b0ad8d67db2f110c911d3f2238c
Marketa Jerabek <102174405+Marki4@users.noreply.github.com>
2024-08-09 10:10:25 -0500

    Update 2_indicators_calculation.R

--- R/2_indicators_calculation.R
+++ R/2_indicators_calculation.R
@@ -1,8 +1,15 @@
 ###Indicators calculation
 
+#IMPORTNAT: the parts on irregularity (in Protection, CP and HTS) 
+#need to be adjusted by each country as the required 
+#documents for regularization are different in each country
+
+#IMPORTANT: poverty line also needs to be adjusted depending on each country's context
+
 main_merged <- main_merged %>%
   
-  # food security with FES
+# Food Security with FES (Poverty Line)
+  
   mutate(
     FCS = (
       FS_D1_Q1 * 2 + FS_D1_Q5 * 1 +
@@ -27,16 +34,13 @@ main_merged <- main_merged %>%
                         R_CSI > 4, 2, FCS_rCSI)
   ) %>%
   mutate(FS_D2 = FCS_rCSI)%>%
-  mutate(
-    FES = FS_D3_Q1 / (FS_D3_Q1 + FS_D3_Q2),
-    Foodexp_4pt = case_when(
-      FES <= .4999999 ~ 1,
-      FES >= .5 & FES <= .64999999 ~ 2,
-      FES >= .65 & FES <= .74999999 ~ 3,
-      FES >= .75 ~ 4
-    )
-  ) %>%
- mutate(FS_D3 = Foodexp_4pt) %>%
+  mutate(spending_pc = (FS_D3_Q1 + FS_D3_Q2) /MH_1) %>%
+  mutate(Foodexp_4pt = case_when(
+    spending_pc >= 109.5 ~ 1,
+    spending_pc >= 64.5 & spending_pc < 109.5 ~ 3,
+    spending_pc < 64.5 ~ 4
+  )) %>%
+  mutate(FS_D3 = Foodexp_4pt) %>%
   mutate(
     stress_coping = case_when(
       FS_D4_Q1 == "already_used_12" |
@@ -89,100 +93,12 @@ main_merged <- main_merged %>%
     CARI_FES = round_half_up(CARI_unrounded_FES)
   ) %>%
   mutate(IND_FS_max = ifelse(CARI_FES > 2, 1, 0))%>%
-
-# food security without FES
-main_merged <- main_merged %>%
-  mutate(
-    FCS = (
-      FS_D1_Q1 * 2 + FS_D1_Q5 * 1 +
-        FS_D1_Q8 * 1 + FS_D1_Q4 * 4 +
-        FS_D1_Q3 * 4 + FS_D1_Q2 * 3 +
-        FS_D1_Q9 * .5 + FS_D1_Q10 * .5
-    )
-  ) %>%
-  mutate(FCG = case_when(FCS <= 28.0 ~ 1, FCS >= 28.5 &
-                           FCS <= 42.0 ~ 2, FCS >= 42.5 ~ 3)) %>%
-  mutate(FCS_4pt = case_when(FCG == 1 ~ 4, FCG == 2 ~ 3, FCG == 3 ~ 1)) %>%
-  mutate(FS_D1 = FCS_4pt) %>%
   
-  mutate(R_CSI = (FS_D2_Q1 * 1 + FS_D2_Q2 * 1 +
-                    FS_D2_Q3 * 1 + FS_D2_Q4 * 3 +
-                    FS_D2_Q5 * 2)) %>%
-  mutate(r_CSI_categories = case_when(R_CSI <= 4 ~ 1, R_CSI > 4 &
-                                        R_CSI <= 18 ~ 2, ... =
-                                        R_CSI >= 19 ~ 3))%>%
-  mutate(
-    FCS_rCSI = ifelse(FCS_4pt %in% c(1, 3, 4), FCS_4pt, NA),
-    FCS_rCSI = ifelse(FCS_4pt == 1 &
-                        R_CSI > 4, 2, FCS_rCSI)
-  ) %>%
-  # mutate(FS_D2 = FCS_rCSI)%>%
-  # mutate(
-  #   FES = FS_D3_Q1 / (FS_D3_Q1 + FS_D3_Q2),
-  #   Foodexp_4pt = case_when(
-  #     FES <= .4999999 ~ 1,
-  #     FES >= .5 & FES <= .64999999 ~ 2,
-  #     FES >= .65 & FES <= .74999999 ~ 3,
-  #     FES >= .75 ~ 4
-  #   )
-  # ) %>%
- #  mutate(FS_D3 = Foodexp_4pt) %>%
-  mutate(
-    stress_coping = case_when(
-      FS_D4_Q1 == "already_used_12" |
-        FS_D4_Q1 == "yes" |
-        FS_D4_Q2 == "already_used_12" |
-        FS_D4_Q2 == "yes" |
-        FS_D4_Q3 == "already_used_12" |
-        FS_D4_Q3 == "yes"|
-        FS_D4_Q8 == "already_used_12" |
-        FS_D4_Q8 == "yes"~ 1,
-      TRUE ~ 0)) %>% 
-  mutate(
-    crisis_coping = case_when(
-      FS_D4_Q4 == "already_used_12" |
-        FS_D4_Q4 == "yes" |
-        FS_D4_Q5 == "already_used_12" |
-        FS_D4_Q5 == "yes" |
-        FS_D4_Q6 == "already_used_12" |
-        FS_D4_Q6 == "yes" ~ 1,
-      TRUE ~ 0)) %>%
-  mutate(
-    emergency_coping = ifelse(
-      FS_D4_Q7 == "already_used_12" |
-        FS_D4_Q7 == "yes" |
-        FS_D4_Q9 == "already_used_12" |
-        FS_D4_Q9 == "yes" |
-        FS_D4_Q10 == "already_used_12" |
-        FS_D4_Q10 == "yes",
-      1,
-      0
-    )
-  ) %>%
-  mutate(
-    stress_coping = recode(stress_coping, "0" = 0, "1" = 2),
-    crisis_coping = recode(crisis_coping, "0" = 0, "1" = 3),
-    emergency_coping = recode(emergency_coping, "0" = 0, "1" = 4),
-    Max_coping_behaviour = pmax(stress_coping, crisis_coping, emergency_coping),
-    Max_coping_behaviour = recode(Max_coping_behaviour, "0" = 1)
-  ) %>%
-  mutate(FS_D4 = Max_coping_behaviour) %>%
-  mutate(
-    Mean_coping_capacity_NoFES = rowMeans(across(c(
-      Max_coping_behaviour
-    )), na.rm = TRUE),
-    
-    CARI_unrounded_NoFES = rowMeans(across(c(
-      FCS_rCSI, Mean_coping_capacity_NoFES
-    )), na.rm = TRUE),
-    
-    CARI_NoFES = round_half_up(CARI_unrounded_NoFES)
-  ) %>%
-  mutate(IND_FS_NoFES = ifelse(CARI_NoFES > 2, 1, 0))   %>%            
-  CARI_Perc <- sum(main_merged$"IND_FS_NoFES")/nrow(main_merged)%>%
-  
+  #ARI_Perc <- sum(main_merged$"IND_FS_max")/nrow(main_merged)
 
-  # health
+
+#Health
+  
   group_by(id_hogar)%>%
   mutate(HE_D1 = case_when(any(HE_D1_Q1 == "yes" &
                                  HE_D1_Q2 == "no",na.rm = TRUE)~ 1,
@@ -193,14 +109,19 @@ main_merged <- main_merged %>%
                                 TRUE ~ 0))%>%
   ungroup()%>% 
   
-  # humanitarian transportation
-  mutate(IND_HT_max = case_when(
-    (HT_D1_Q1_walking == 1 |
-       HT_D1_Q1_bike == 1) & HT_D1_Q2 == "30m-plus"~ 1,
-    TRUE ~ 0
-  )) %>%
+# Humanitarian Transportation
   
-  # integration
+  mutate(
+    IND_HT_max = case_when(
+      (HT_D1_Q1_walking == 1 | HT_D1_Q1_bike == 1) & HT_D1_Q2 == "30m-plus" |
+        HT_D1_Q1_informal_particular_transport == 1 |
+        HT_D1_Q1_govt_ngo_transport == 1 |
+        HT_D1_Q1_autostop == 1 ~ 1,
+      TRUE ~ 0
+    )
+  ) %>%
+  
+# Integration
   
   group_by(id_hogar)%>%
   mutate(
@@ -219,7 +140,7 @@ main_merged <- main_merged %>%
   mutate(IND_INT_D1_max = ifelse(INT_D1 == 1 | INT_D2 == 1, 1, 0))%>%
   ungroup()%>% 
   
-  # nutrition
+# Nutrition
   
   mutate(
     IND_NUT_D1 = case_when(
@@ -280,13 +201,23 @@ main_merged <- main_merged %>%
   mutate(IND_NUT_D2 = ifelse(NUT_D4 == 1 | NUT_D8 == 1, 1, 0))%>%  
   mutate(IND_NUT_D3 = ifelse(NUT_D10 == 1 | NUT_D5 == 1, 1, 0))%>%  
   
-  # protection
+# Protection
+  
   mutate(
     PRO_D1 = ifelse(PROT_D1_Q1 != "none", 1, 0),
     PRO_D2	= ifelse(PROT_D2_Q1 != "none", 1, 0),
     PRO_D3 = case_when(PROT_D3_Q1 == "yes" & PROT_D3_Q3 == "no"~ 1,
                        TRUE ~ 0),
-    IND_PRO_D3_max = ifelse(is.na(MH_6_no_valid_document), 0, ifelse(MH_6_no_valid_document == 1, 1, 0)),
+    IND_PRO_D3_max = if_else(
+      coalesce(MH_6_identification_document_destination, 0) != 1 &
+        coalesce(MH_6_permission_destination, 0) != 1 &
+        coalesce(MH_6_temporal_residency_destination, 0) != 1 &
+        coalesce(MH_6_temporal_protection_destination, 0) != 1 &
+        coalesce(MH_6_citizenship_destination, 0) != 1 &
+        coalesce(MH_6_asylum_seeker_documents, 0) != 1,
+      1,  # Set to 1 if all document fields are not 1 (meaning no documents are present)
+      0   # Set to 0 if at least one document field is 1 (meaning at least one document is present)
+    ),
     PRO_D5 = case_when((PROT_D5_Q1 == "yes" |
                           PROT_D5_Q1 == "prefer_not_answer") & (
                             PROT_D5_Q2_threat == 1 |
@@ -301,35 +232,85 @@ main_merged <- main_merged %>%
     )
   ) %>%
   
-  # child protection
-  mutate(IND_PRO_CP_max = case_when(
-      CP_D1_Q1 != "none"  &
-      CP_D1_Q2 == "no"~ 1,
-    TRUE ~ 0
-  )) %>%
+# Child Protection
+  
+  group_by(id_hogar) %>%
+  mutate(age1_rounded = floor(age1)) %>%
+  mutate(
+    irregular_child = if_else(
+      age1_rounded < 18 &
+        (
+          coalesce(MH_6_identification_document_destination, 0) != 1 &
+            coalesce(MH_6_permission_destination, 0) != 1 &
+            coalesce(MH_6_temporal_residency_destination, 0) != 1 &
+            coalesce(MH_6_temporal_protection_destination, 0) != 1 &
+            coalesce(MH_6_citizenship_destination, 0) != 1 &
+            coalesce(MH_6_asylum_seeker_documents, 0) != 1
+        ),
+      1,  # Set to 1 if all document fields are not 1 (meaning no documents are present)
+      0   # Set to 0 if at least one document field is 1 (meaning at least one document is present)
+    )
+  )%>%
+  ungroup() %>%
+  mutate(
+    IND_PRO_CP_max = case_when(
+      CP_D1_Q1 != "none" | irregular_child == 1 ~ 1,
+      TRUE ~ 0
+    )
+  )%>%
   
   
-  # gender based violence
+# Gender Based Violence
+    
   group_by(id_hogar) %>%
   mutate(
-    GBV_D1 = ifelse(any(GBV_D1_Q1 == "yes",na.rm = TRUE), 1, 0),
-    GBV_D3 = case_when(any(GBV_D3_Q1 == 4 | GBV_D3_Q1 == 5,na.rm = TRUE)~ 1,
-                       TRUE ~ 0)
-  ) %>%
+    GBV_D1 = ifelse(any(GBV_D1_Q1 == "yes" | GBV_D1_Q1 == "prefer_not_answer", na.rm = TRUE), 1, 0),
+    GBV_D3 = case_when(
+      any(GBV_D3_Q1 == 4 | GBV_D3_Q1 == 5) |
+        any(str_detect(CP_D1_Q1, "physical_psychological_violence") | str_detect(CP_D1_Q1, "sex_for_services_goods")) |
+        any(str_detect(PROT_D1_Q1, "physical_psychological_violence") | str_detect(PROT_D1_Q1, "prefer_not_answer")) |
+        any(str_detect(HTS_D1_Q1, "yes") | str_detect(HTS_D1_Q1, "prefer_not_answer")) |
+        any(str_detect(HTS_D1_Q2, "yes") | str_detect(HTS_D1_Q2, "prefer_not_answer")) |
+        HTS_D2_Q1_against_will_activities == 1 ~ 1,
+      TRUE ~ 0
+    )
+  )%>%
   
-  # H&T
+# Human Trafficking and Smuggling
+    
   mutate(
-    HTS_D1 = case_when(HTS_D1_Q1 == "yes" | HTS_D1_Q2 == "yes"~ 1,
-                       TRUE ~ 0),
-    HTS_D2 = case_when(HTS_D2_Q1_unpaid_longer_work == 1 |
-                         HTS_D2_Q1_conditioned_work == 1 |  
-                         HTS_D2_Q1_risky_work == 1 |  
-                         HTS_D2_Q1_against_will_activities == 1 |
-                         HTS_D2_Q1_unpleasant_treatment == 1 | 
-                         HTS_D2_Q1_less_paid == 1 |
-                         HTS_D2_Q1_no_pay == 1 | 
-                         HTS_D2_Q1_prefer_not_answer == 1~ 1,
-                       TRUE ~ 0))%>%
+    HTS_D1 = case_when(
+      HTS_D1_Q1 == "yes" | HTS_D1_Q2 == "yes" ~ 1,
+      TRUE ~ 0
+    )
+  ) %>%
+  group_by(id_hogar) %>%
+  mutate(
+    irregular_person = any(coalesce(MH_6_identification_document_destination, 0) != 1 &
+                             coalesce(MH_6_permission_destination, 0) != 1 &
+                             coalesce(MH_6_temporal_residency_destination, 0) != 1 &
+                             coalesce(MH_6_temporal_protection_destination, 0) != 1 &
+                             coalesce(MH_6_citizenship_destination, 0) != 1 &
+                             coalesce(MH_6_asylum_seeker_documents, 0) != 1
+    ),
+    1,  # Set to 1 if all document fields are not 1 (meaning no documents are present)
+    0   # Set to 0 if at least one document field is 1 (meaning at least one document is present)
+  ) %>%
+  ungroup() %>%
+  mutate(
+    HTS_D2 = case_when(
+      (HTS_D2_Q1_unpaid_longer_work == 1 |
+         HTS_D2_Q1_conditioned_work == 1 |  
+         HTS_D2_Q1_risky_work == 1 |  
+         HTS_D2_Q1_against_will_activities == 1 |
+         HTS_D2_Q1_unpleasant_treatment == 1 | 
+         HTS_D2_Q1_less_paid == 1 |
+         HTS_D2_Q1_no_pay == 1 | 
+         HTS_D2_Q1_prefer_not_answer == 1) &
+        irregular_person ~ 1,
+      TRUE ~ 0
+    )
+  )%>%
   
   mutate(IND_PRO_D1_max = case_when(PRO_D1 == 1 | PRO_D2 == 1~ 1,
                                     TRUE ~ 0))%>%  
@@ -341,7 +322,8 @@ main_merged <- main_merged %>%
                                      TRUE ~ 0))%>% 
   ungroup()%>% 
   
-  # shelter
+# Shelter
+    
   mutate(
     SHE_D1 = case_when(
       SHE_D1_Q1 == "street" |
@@ -379,7 +361,8 @@ main_merged <- main_merged %>%
                                     TRUE ~ 0))%>% 
   ungroup()%>% 
   
-  # wash
+# WASH
+    
   mutate(
     WA_D1 = case_when(
       (WASH_D1_Q1 == "rain_water" |
@@ -449,7 +432,7 @@ main_merged <- main_merged %>%
   mutate(IND_WA_D3_max = ifelse(WA_D11 == 1 | WA_D8 == 1, 1, 0))%>% 
   ungroup()%>% 
   
-  # education
+# Education
   
   mutate(
     EDU_D1 = ifelse(EDU_D1_Q1 == "no", 1, 0),
